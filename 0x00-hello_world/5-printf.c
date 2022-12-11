0afe9254fdd20cdd79aa03796d33c13c49e80b20
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-11 14:43:10 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
-#include <stdio.h>                                                                                                                                             
-int main(void) {
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
-}  
+	return (0);
+}
