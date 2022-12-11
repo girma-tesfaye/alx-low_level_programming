a32d7047e90bb07679b83a762431c64d35bcfd67
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-11 15:46:25 -0800

    6-sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu. \n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu. \n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu. \n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu. \n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu. \n", (unsigned long)sizeof(f));
+	return (0);
+}
