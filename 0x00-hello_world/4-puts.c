f316ad9d1c1e2d1080b4512693b3cbe7d537e991
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-11 13:15:27 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
 #include<stdio.h>
-int main (void) {printf("with proper grammar, but the outcome is a piece of art,\n");return 0;}
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
