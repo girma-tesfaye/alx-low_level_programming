7698d598cdad084315974839fedf9f3371e97f6c
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-12 07:05:21 -0800

    2-print-alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
