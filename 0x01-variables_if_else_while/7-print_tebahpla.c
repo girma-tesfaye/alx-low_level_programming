100fa51f54c4ea821c4492887e244bba1aa7811a
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-12 12:05:22 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
