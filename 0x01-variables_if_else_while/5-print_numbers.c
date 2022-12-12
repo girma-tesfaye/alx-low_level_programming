938c7d06b5ea2868d2f62a57136062b25963d921
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-12 11:43:50 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poin
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
