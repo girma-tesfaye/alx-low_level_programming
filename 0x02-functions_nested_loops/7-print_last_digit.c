7f76af79856919e6b829c17fc3dab4b1bcb4a539
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-13 15:38:32 -0800

    last-digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,13 @@
 
 int print_last_digit(int n)
 {
-	char last_digit = n % 10;
+	int last_digit;
 
-	_putchar(last_digit);
+	if (n < 0)
+		last_digit = (-1 * (n % 10));
+	else
+		last_digit = n % 10;
+	_putchar(last_digit + '0');
 	return (0);
 }
 
