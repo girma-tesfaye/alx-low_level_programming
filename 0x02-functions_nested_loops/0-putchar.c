181e331a15ffbae35f848ebefac07086b3419db6
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-13 11:27:08 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -9,12 +8,14 @@
 
 int main(void)
 {
-	char *string = "_putchar";
-	int i;
-	int length = strlen(string);
-
-	for (i = 0; i <= length; i++)
-		putchar(string[i]);
-	putchar("\n");
+	_putchar('_');
+       	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
