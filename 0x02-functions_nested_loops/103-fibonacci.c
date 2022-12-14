a352152db6581cab5f0a26c59e024fae9a22b6b1
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-14 13:31:52 -0800

    fibonacci-add

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,19 +9,20 @@
 int main(void)
 {
 	int n;
-	long int sum, fib[50];
+	long int sum = 2, fib[50];
 
 	fib[0] = 1;
 	fib[1] = 2;
-	fib[n] = fib[n - 1] + fib[n - 2];
 
 	for (n = 2; n <= 50; n++)
 	{
+		fib[n] = fib[n - 1] + fib[n - 2];
 		if (fib[n] % 2 == 0 && fib[n] <= 4000000)
 		{
 			sum += fib[n];
 			printf("%ld\n", sum);
 		}
 	}
+	return (0);
 }
 
