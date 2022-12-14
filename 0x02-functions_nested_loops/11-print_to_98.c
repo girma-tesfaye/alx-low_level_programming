2908b222bf77c8e6ccf133aa83f9fb1b68f1b55a
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-14 00:22:06 -0800

    natural-numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry
+ * @n: passed integer argument
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int r;
+
+	while (n > 0 && n <= 98)
+	{
+		r = n++;
+
+		printf("%d, \n", r);
+	}
+}
