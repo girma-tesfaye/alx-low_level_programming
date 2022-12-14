35f7d4c8aea8b511d19003f685274c7ae96e4f14
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-14 02:21:46 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	long int fibonacci[50], res = 2;
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
+	for (n = 2; n < 50; n++)
+	{
+		fibonacci[n] = fibonacci[n - 1] + fibonacci[n - 2];
+	
+		if ((fibonacci[n] % 2) == 0 && fibonacci[n] < 4000000)
+			sum += fibonacci[n];
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
