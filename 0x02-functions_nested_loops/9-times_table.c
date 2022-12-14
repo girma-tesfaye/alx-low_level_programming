3850042304210ab851d4c52ec27a1c652327b1b6
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-13 23:41:13 -0800

    times-table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Entry
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+
+			if ((k / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(k + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j == 9)
+				{
+					continue;
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
