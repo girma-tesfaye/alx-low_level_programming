8c1318ba3fbe87c1cd1e60c3829a2ab603b49785
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-14 02:36:52 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n;
 
-	long int fibonacci[50], res = 2;
+	long int fibonacci[50];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
@@ -20,8 +20,7 @@ int main(void)
 		fibonacci[n] = fibonacci[n - 1] + fibonacci[n - 2];
 
 		if ((fibonacci[n] % 2) == 0 && fibonacci[n] < 4000000)
-			res += fibonacci[n];
+			printf("%lld, ", fibonacci[n]);
 	}
-	printf("%ld\n", res);
 	return (0);
 }
