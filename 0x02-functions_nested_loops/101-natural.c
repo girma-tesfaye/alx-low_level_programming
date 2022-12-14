46cf100c91355937395eb18bc8c08503203e5c80
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-14 02:04:26 -0800

    naturals-sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, sum;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == %)
+			sum + = n;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
