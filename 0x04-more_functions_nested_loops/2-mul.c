a1fc71ebcff855e55586e216e20f55ec08e3459d
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-15 02:06:58 -0800

    mulplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: the integer argument a
+ * @b: the integer argument b
+ * Return: 0 upon successful execution
+ */
+
+int mul(int a, int b)
+{
+	int mul_res = a * b;
+	_putchar(mul_res);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
