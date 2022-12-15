ab3e4306989f0047d8b5a11f383a2dae4706e36f
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-15 06:46:57 -0800

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,22 +4,15 @@
  * print_most_numbers - Entry
  * Return: void
  */
-
 void print_most_numbers(void)
 {
-	int n = 0;
+	int n =	'0';
 
-	while (n >= 0 && n <= 9)
+	while (n <= '9')
 	{
-		if (n == 2 || n == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar('0' + n);
-			n++;
-		}
+		if (n != '2' && n != '4')
+			_putchar(n);
+		n++;
 	}
 	_putchar('\n');
 }
