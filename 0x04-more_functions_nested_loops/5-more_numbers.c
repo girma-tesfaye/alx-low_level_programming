805715a0a22bd364eb0ca146bbddbffad06c2c86
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-15 07:40:22 -0800

    10=times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,22 @@
 
 void more_numbers(void)
 {
-	int n = '0', i;
+	int n = 0, i;
 
-	while (n <= '9')
+	while (n <= 9)
 	{
-		for (i = '0'; i <= '9'; i++)
-			_putchar(i);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{	
+				_putchar('0' + i / 10);
+				_putchar('0' + i % 10);
+			}
+			else
+			{
+				_putchar('0' +  i);
+			}
+		}
 		n++;
 		_putchar('\n');
 	}
