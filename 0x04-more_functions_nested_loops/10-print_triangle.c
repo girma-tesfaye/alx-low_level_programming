fc14fd1daa51eaec8a8d0c9430b64e5781d803d3
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-16 11:43:18 -0800

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+/**
+ * print_triangle - custom sized triangle printer
+ * @size: triangle base parameter
+ * Return: void
+ */
+void print_triangle(int size)
+{
+
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,14 @@ int main(void)
 
 	while (n < 100)
 	{
-		if (n % 3 == 0)
-		{
-			if (n % 5 == 0)
-				printf("FizzBuzz ");
-			else
-				printf("Fizz ");
-		}
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
 		else if (n % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", n);
-		}
 		n++;
 	}
 	printf("Buzz\n");
