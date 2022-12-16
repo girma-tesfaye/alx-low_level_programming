00c9b2b0cc86c5d99047fc212cfe2c6a3c177b58
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-16 07:24:25 -0800

    print_square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,17 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		if (size > 0)
+		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
 				_putchar(35);
 			_putchar('\n');
 		}
-		else
-		{
-			_putchar('\n');
-		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
