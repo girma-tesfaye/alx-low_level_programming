e01a150679ca44e5a1bf11b80fef3d92c538d262
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-16 14:58:04 -0800

    largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,5 @@
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+int largest_number(int, int, int);
 #endif /* MAIN_H */
