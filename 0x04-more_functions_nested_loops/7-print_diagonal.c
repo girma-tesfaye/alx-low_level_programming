36ff66a9eaba15e6185a44bef19be80090424821
girma-tesfaye <girmatesfaye80@gmail.com>
2022-12-16 06:00:24 -0800

    print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal line drawer function
+ * @n: number parameter to specify line length
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	char diag_line = 92, diag_space = 32;
+	int i = 1;
+
+	while (i <= n)
+	{
+		if (n < 1)
+			_putchar('\n');
+		else
+		{
+			for (i = 1; i <= n; i++)
+				_putchar(diag_space);
+			_putchar(diag_line);
+		}
+		i++;
+		_putchar('\n');
+	}
+}
